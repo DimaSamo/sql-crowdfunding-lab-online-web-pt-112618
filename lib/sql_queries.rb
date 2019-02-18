752689fa6b5722bf74c84e6a26e02f9deccf2792
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-18 07:17:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,11 +41,11 @@ ORDER BY pledged_amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, SUM(pledges.amount)
+"SELECT projects.category, pledges.amount
 FROM projects
 INNER JOIN pledges
 on projects.id=pledges.project_id
-GROUP BY projects.category
+WHERE projects.category='music'
 ;"
 end
 
