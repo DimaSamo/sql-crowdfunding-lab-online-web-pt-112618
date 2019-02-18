d8a6ebcc26e5fcd8f2f57f3b0fecf9cd70edb13e
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-18 07:18:36 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,61 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("a", "ab", 100, "1/1/2011", "1/1/2012");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("b", "cd", 200, "2/2/2012", "2/2/2013");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("c", "de", 300, "3/3/2013", "3/3/2014");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("d", "ef", 400, "4/4/2014", "4/4/2015");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("e", "fg", 500, "5/5/2015", "5/5/2016");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("f", "gh", 600, "6/6/2016", "6/6/2017");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("g", "hi", 700, "7/7/2017", "7/7/2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("h", "ij", 800, "8/8/2018", "8/8/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("i", "jk", 900, "9/9/2019", "9/9/2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("j", "kl", 1000, "10/10/2020", "10/10/2021");
+INSERT INTO users (name, age) VALUES ("A", 20);
+INSERT INTO users (name, age) VALUES ("B", 21);
+INSERT INTO users (name, age) VALUES ("C", 22);
+INSERT INTO users (name, age) VALUES ("D", 23);
+INSERT INTO users (name, age) VALUES ("E", 24);
+INSERT INTO users (name, age) VALUES ("F", 25);
+INSERT INTO users (name, age) VALUES ("G", 26);
+INSERT INTO users (name, age) VALUES ("H", 27);
+INSERT INTO users (name, age) VALUES ("I", 28);
+INSERT INTO users (name, age) VALUES ("J", 29);
+INSERT INTO users (name, age) VALUES ("K", 30);
+INSERT INTO users (name, age) VALUES ("L", 31);
+INSERT INTO users (name, age) VALUES ("M", 32);
+INSERT INTO users (name, age) VALUES ("N", 33);
+INSERT INTO users (name, age) VALUES ("O", 34);
+INSERT INTO users (name, age) VALUES ("P", 35);
+INSERT INTO users (name, age) VALUES ("Q", 36);
+INSERT INTO users (name, age) VALUES ("R", 37);
+INSERT INTO users (name, age) VALUES ("S", 38);
+INSERT INTO users (name, age) VALUES ("T", 39);
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
