65c76a527544718d951ec0bac1580df986389bed
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-18 06:25:19 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("a", "ab", 100, "1/1/2011", "1/1/2012");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("b", "cd", 200, "2/2/2012", "2/2/2013");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("c", "de", 300, "3/3/2013", "3/3/2014");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("d", "ef", 400, "4/4/2014", "4/4/2015");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("e", "fg", 500, "5/5/2015", "5/5/2016");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("f", "gh", 600, "6/6/2016", "6/6/2017");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("g", "hi", 700, "7/7/2017", "7/7/2018");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("h", "ij", 800, "8/8/2018", "8/8/2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("i", "jk", 900, "9/9/2019", "9/9/2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("j", "kl", 1000, "10/10/2020", "10/10/2021");
