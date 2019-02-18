318cd591ab24a1f601ade94c2a8db9ee9e98f4d8
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-18 07:08:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,9 @@ HAVING over_goal>=0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-""
+"SELECT users.name SUM(pledges.amount) AS pledged_amount
+FROM pledges
+"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
